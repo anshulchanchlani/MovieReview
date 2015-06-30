3084fd967d478619a80064b700f537ecc1697bed
acvoltage <anshulchanchlani@gmail.com>
2015-06-30 17:11:29 +0530

    Add paperclip for image upload
    Movies now appear with their images

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "400x600#" }
+
+  Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.9.1-Q16'
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
